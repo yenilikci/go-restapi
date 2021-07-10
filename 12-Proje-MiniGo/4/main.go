71c79e9f8068cd8a1c9cdb8ce59b2d792eee64f7
yenilikci <m.melihcelik017@outlook.com>
2021-07-11 00:42:05 +0300

    gorilla mux, mux.NewRouter(), mux.Vars()

---
+++ 12-Proje-MiniGo/4/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type API struct {
+	Message string "json:message"
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	urlParams := mux.Vars(r)
+	id := urlParams["id"]
+	messageConcat := "Kullanıcı ID: " + id
+
+	message := API{messageConcat}
+	output, err := json.Marshal(message)
+
+	if err != nil {
+		fmt.Println("Hata oluştu")
+	}
+	fmt.Fprintf(w, string(output))
+}
+
+func main() {
+	gorillaRoute := mux.NewRouter()
+	//gorillaRoute.HandleFunc("/api/user/{id}", Hello)
+	gorillaRoute.HandleFunc("/api/user/{id:[0-9]+}", Hello) //numeric val filter
+	http.Handle("/", gorillaRoute)
+	http.ListenAndServe(":8080", nil)
+}
